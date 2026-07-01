76221f3255154aedaae44e2b76280f621d313ef5
Leandro <leandrotlz@gmail.com>
2026-07-01 14:24:31 +0200

    Create visual nodes.

---
+++ visualnode.js
@@ -0,0 +1,63 @@
+const computedStyles = window.getComputedStyle(document.documentElement);
+const gridSize = parseFloat(computedStyles.getPropertyValue('--grid-size')) || 40;
+
+const TITLE_HEIGHT = 30;
+const PIN_LOCATION = TITLE_HEIGHT + 16;
+const PIN_SEPARATION = 20;
+const PIN_TEXT_OFFSET_X = 12;
+const PIN_TEXT_OFFSET_Y = 4;
+
+export class VisualNode {
+    constructor(type, title, gridX, gridY, width, height, inputPins, outputPins) {
+        this.type = type;
+        this.title = title;
+
+        this.x = gridX * gridSize;
+        this.y = gridY * gridSize;
+        this.width = width * gridSize;
+        this.height = height * gridSize;
+
+        this.inputPins = inputPins;
+        this.outputPins = outputPins;
+    }
+
+    drawPin(ctx, displayText, x, y, side) {
+        ctx.fillStyle = computedStyles.getPropertyValue(`--pin-${side}-color`).trim();
+        ctx.beginPath();
+        ctx.arc(x, y, 5, 0, Math.PI * 2);
+        ctx.fill();
+
+        ctx.font = computedStyles.getPropertyValue('--node-body-font').trim();
+        ctx.fillStyle = computedStyles.getPropertyValue('--node-body-color').trim();
+        if (side === "out") {
+            const textWidth = ctx.measureText(displayText).width;
+            ctx.fillText(displayText, x - textWidth - PIN_TEXT_OFFSET_X, y + PIN_TEXT_OFFSET_Y);
+        } else {
+            ctx.fillText(displayText, x + PIN_TEXT_OFFSET_X, y + PIN_TEXT_OFFSET_Y);
+        }
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = computedStyles.getPropertyValue(`--node-${this.type}-body`).trim();
+        ctx.beginPath();
+        ctx.roundRect(this.x, this.y, this.width, this.height, 10);
+        ctx.fill();
+
+        ctx.fillStyle = computedStyles.getPropertyValue(`--node-${this.type}-bg`).trim();
+        ctx.beginPath();
+        ctx.roundRect(this.x, this.y, this.width, TITLE_HEIGHT, 10);
+        ctx.fill();
+
+        ctx.font = computedStyles.getPropertyValue('--node-title-font').trim();
+        ctx.fillStyle = computedStyles.getPropertyValue('--node-title-color').trim();
+        ctx.fillText(this.title, this.x + 12, this.y + 19);
+
+        for (let i = 0; i < this.inputPins; i++) {
+            this.drawPin(ctx, "0", this.x, this.y + PIN_LOCATION + (i * PIN_SEPARATION), "in");
+        }
+
+        for (let i = 0; i < this.outputPins; i++) {
+            this.drawPin(ctx, "0", this.x + this.width, this.y + PIN_LOCATION + (i * PIN_SEPARATION), "out");
+        }
+    }
+}
--- visualrpn.js
+++ visualrpn.js
@@ -1,4 +1,5 @@
 import { drawGrid, gridSize } from './canvasgrid.js';
+import { VisualNode } from './visualnode.js';
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
@@ -11,13 +12,33 @@ let zoomLevel = 1.0;
 const MIN_ZOOM = 0.3;
 const MAX_ZOOM = 2.0;
 
+let nodeList = [];
+
 function drawCanvas() {
     drawGrid(canvas, ctx, panOffset, zoomLevel);
+    nodeList.forEach(node => node.draw(ctx));
+}
+
+function resetNodes() {
+    const nodeWidth = 3;
+    const nodeHeight = 2;
+
+    const canvasGridWidth = Math.floor(window.innerWidth / gridSize);
+    const targetGridX = canvasGridWidth - 1 - nodeWidth;
+
+    const canvasGridHeight = Math.floor(window.innerHeight / gridSize);
+    const targetGridY = Math.floor((canvasGridHeight - nodeHeight) / 2);
+
+    nodeList = [];
+    nodeList.push(new VisualNode("output", "Output", targetGridX, targetGridY, nodeWidth, nodeHeight, 1, 0));
+    nodeList.push(new VisualNode("operator", "Operator", targetGridX - 4, targetGridY, nodeWidth, nodeHeight, 2, 1));
 }
 
 function resizeCanvas() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
+    resetNodes();
     drawCanvas();
 }
 window.addEventListener('resize', resizeCanvas);
