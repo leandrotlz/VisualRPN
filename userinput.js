107e0c5a322b6dbabf02d5ec59598e1054fd0082
Leandro <leandrotlz@gmail.com>
2026-07-03 00:57:03 +0200

    Pinch to zoom.

--- userinput.js
+++ userinput.js
@@ -1,9 +1,9 @@
 export class UserInput {
     constructor(canvas, callbacks) {
-        this.isPanning = false;
-        this.panStart = { x: 0, y: 0 };
-
         this.canvas = canvas;
+
+        this.pan = { x: 0, y: 0, active: false }
+        this.pinch = { x: 0, y: 0, initialDistance: 0, initialZoom: 0 }
 
         this.getViewport = callbacks.getViewport || (() => ({ panOffset: { x: 0, y: 0 }, zoomLevel: 1 }));
         this.getNodeAtPoint = callbacks.getNodeAtPoint || (() => null);
@@ -13,6 +13,14 @@ export class UserInput {
         this.onNodeSelected = callbacks.onNodeSelected || (() => {});
 
         this.addListeners();
+    }
+
+    setPanningState(isPanning, x, y) {
+        this.pan.active = isPanning;
+        if (!isPanning) return;
+
+        this.pan.x = x;
+        this.pan.y = y;
     }
 
     addListeners() {
@@ -26,26 +34,23 @@ export class UserInput {
                     this.onNodeSelected(node);
                 } else {
                     this.onNodeSelected(null);
-                    this.isPanning = true;
-                    this.panStart.x = e.clientX - panOffset.x;
-                    this.panStart.y = e.clientY - panOffset.y;
+                    this.setPanningState(true, e.clientX - panOffset.x, e.clientY - panOffset.y);
                 }
             }
             else if (e.button === 1) {
-                this.isPanning = true;
-                this.panStart.x = e.clientX - panOffset.x;
-                this.panStart.y = e.clientY - panOffset.y;
+                // Middle button always pans and doesn't change node selection.
+                this.setPanningState(true, e.clientX - panOffset.x, e.clientY - panOffset.y);
             }
         });
 
         this.canvas.addEventListener('mousemove', (e) => {
-            if (this.isPanning) {
-                this.onPan(e.clientX - this.panStart.x, e.clientY - this.panStart.y);
+            if (this.pan.active) {
+                this.onPan(e.clientX - this.pan.x, e.clientY - this.pan.y);
             }
         });
 
         window.addEventListener('mouseup', (e) => {
-            this.isPanning = false;
+            this.setPanningState(false);
         });
 
         this.canvas.addEventListener('wheel', (e) => {
@@ -55,29 +60,39 @@ export class UserInput {
         });
 
         this.canvas.addEventListener('touchstart', (e) => {
-            const { panOffset } = this.getViewport();
+            const { panOffset, zoomLevel } = this.getViewport();
 
-            // Assume one touch for now, figure out pinch-to-zoom later.
-            const touch = e.touches[0];
-            this.isPanning = true;
-            this.panStart.x = touch.clientX - panOffset.x;
-            this.panStart.y = touch.clientY - panOffset.y;
+            if (e.touches.length === 1) {
+                const touch = e.touches[0];
+                this.setPanningState(true, touch.clientX - panOffset.x, touch.clientY - panOffset.y);
+            } else if (e.touches.length === 2) {
+                this.setPanningState(false);
+                // Store the midpoint between the two touches to zoom around it.
+                this.pinch.x = (e.touches[0].clientX + e.touches[1].clientX) / 2;
+                this.pinch.y = (e.touches[0].clientY + e.touches[1].clientY) / 2;
+                this.pinch.initialDistance = Math.hypot(e.touches[0].clientX - e.touches[1].clientX, e.touches[0].clientY - e.touches[1].clientY);
+                this.pinch.initialZoom = zoomLevel;
+            }
         }, { passive: false });
 
         this.canvas.addEventListener('touchmove', (e) =>{
-            if (this.isPanning) {
+            if (e.touches.length === 1 && this.pan.active) {
                 e.preventDefault();
                 const touch = e.touches[0];
-                this.onPan(touch.clientX - this.panStart.x, touch.clientY - this.panStart.y);
+                this.onPan(touch.clientX - this.pan.x, touch.clientY - this.pan.y);
+            } else if (e.touches.length === 2 && this.pinch.initialDistance > 0) {
+                const distance = Math.hypot(e.touches[0].clientX - e.touches[1].clientX, e.touches[0].clientY - e.touches[1].clientY);
+                const zoom = this.pinch.initialZoom * (distance / this.pinch.initialDistance);
+                this.onZoom(zoom, this.pinch.x, this.pinch.y, false);
             }
         }, { passive: false });
 
         this.canvas.addEventListener('touchend', (e) => {
-            this.isPanning = false;
+            this.setPanningState(false);
         }, { passive: false });
 
         this.canvas.addEventListener('touchcancel', (e) => {
-            this.isPanning = false;
+            this.setPanningState(false);
         }, { passive: false });
     }
 }
